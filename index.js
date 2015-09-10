6a433a263521a3b82a82b98dab3419b0155670c8
Chad Hietala <chadhietala@gmail.com>
2015-09-09 22:01:15 -0700

    Fix falcor path

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ module.exports = {
   name: 'ember-falcor',
   init: function() {
     this.ext = process.env.EMBER_ENV === 'development' ? '.js' : '.min.js';
-    var assets_path = require('path').join('falcor', 'dist', 'falcor.browser' + this.ext);
-    this.treePaths['vendor'] = require.resolve('falcor').replace(assets_path, '');
+    this.treePaths['vendor'] = this.nodeModulesPath + path.sep;
   },
   included: function () {
     this.app.import('vendor/falcor/dist/falcor.browser' + this.ext, {
